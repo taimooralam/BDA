786cc9d96a71e71723b9453f1056d160c9457d42
Taimoor <taimoor.alam12@gmail.com>
2018-07-03 09:26:28 +0200

    first draft task01 and task02

--- src/task01.cc
+++ src/task01.cc
@@ -19,20 +19,54 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //
+#include <inttypes.h>
 
 #include <vigra/multi_array.hxx>
 #include <vigra/impex.hxx>
+#include <vigra/imageinfo.hxx>
+#include <vigra/stdimagefunctions.hxx>
 
 #include <iostream>
 #include <string>
+
+using namespace vigra;
+
+//const string -- recommended
+const std::string c_GLOBAL_SOURCE_IMAGE_PATH = "/home/alam/Documents/code/BioDataAnalysis-WorkAssessment-Vigra/images/bDZ17-1I_wE02_s7_z1_t1_cGFP_u001.tif";
+const std::string c_LOCAL_SOURCE_IMAGE_PATH = "../images/bDZ17-1I_wE02_s7_z1_t1_cGFP_u001.tif";
+const std::string c_DESTINATION_IMAGE_PATH = "../images/task01_destination_image.tif";
+
+const std::pair<int, int> c_from(400,500);
+const std::pair<int, int> c_to(900,850);
+
 
 int main(int argc, char** argv)
 {
     std::cout << "Task 01 started" << std::endl;
 
     // You can add your code here
-
-
-    std::cout << "Task 01 finished successfully" << std::endl;
+	std::cout << "(supported formats: " << vigra::impexListFormats() << " ) " << std::endl;
+	
+	ImageImportInfo v_info(c_LOCAL_SOURCE_IMAGE_PATH.c_str());
+		
+	try{
+		//create array view for the whole input image
+		MultiArray<2, UInt8> v_in(v_info.width(), v_info.height());
+		
+		importImage(v_info, v_in);
+		
+		//create an array view for the desired subregion
+		MultiArray<2, UInt8> v_out = v_in.subarray(Shape2(c_from.first,c_from.second) , Shape2(c_to.first,c_to.second));
+		
+		exportImage(v_out, c_DESTINATION_IMAGE_PATH.c_str());
+		
+		std::cout << "Task 01 ended" << std::endl;
+	
+	}
+	catch(std::exception & a_e){
+		std::cout << a_e.what() << std::endl;
+		return -1;
+	}
+	
     return 0;
 }
--- src/task02.cc
+++ src/task02.cc
@@ -22,15 +22,49 @@
 
 #include <vigra/multi_array.hxx>
 #include <vigra/impex.hxx>
+#include <vigra/convolution.hxx>
 
 #include <iostream>
 #include <string>
+
+using namespace vigra;
+
+const std::string c_LOCAL_SOURCE_IMAGE_PATH = "../images/bDZ17-1I_wE02_s7_z1_t1_cDAPI_u001.tif";
+const std::string c_DESTINATION_IMAGE_PATH = "../images/task02_destination_image.tif";
 
 int main(int argc, char** argv)
 {
     std::cout << "Task 02 started" << std::endl;
 
     // You can add your code here
+	
+	// You can add your code here
+	std::cout << "(supported formats: " << vigra::impexListFormats() << " ) " << std::endl;
+	
+	ImageImportInfo v_info(c_LOCAL_SOURCE_IMAGE_PATH.c_str());
+		
+	try{
+		//create array view for the whole input image
+		MultiArray<2, UInt8> v_in(v_info.width(), v_info.height());
+		
+		//import the input image
+		importImage(v_info, v_in);
+		
+		//create the output image
+		MultiArray<2, UInt8> v_out(v_info.width(), v_info.height());
+		
+		
+		//gaussian smooth with scale 3.0
+		gaussianSmoothing(v_in, v_out, 3.0);
+		
+		exportImage(v_out, c_DESTINATION_IMAGE_PATH.c_str());
+		
+	
+	}
+	catch(std::exception & a_e){
+		std::cout << a_e.what() << std::endl;
+		return -1;
+	}
 
 
     std::cout << "Task 02 finished successfully" << std::endl;
